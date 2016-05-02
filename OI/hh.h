94aa50c2a89bfd265499d78a82c8dd55ac456136
Tommy Zng <1319524930@qq.com>
2016-05-02 22:35:14 +0800

    Create hh.h

---
+++ OI/hh.h
@@ -0,0 +1,27 @@
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+#include<cassert>
+#include<cstdlib>
+#include<algorithm>
+typedef int I;
+typedef long long L;
+#define _v void
+#define _r return
+#define il inline
+#define _w while
+#define _f for
+#define _A assert
+#define _B(...) {__VA_ARGS__;break;}
+#define _C(...) {__VA_ARGS__;continue;}
+#define _D(...) fprintf(stderr,##__VA_ARGS__),fflush(stderr);
+#define _E(...) exit(__VA_ARGS__);
+#define _F _A(0)
+#ifdef _FIO
+#define fp(x) freopen(#x ".in","r",stdin),freopen(#x ".out","w",stdout);
+#else
+#define fp(x) 
+#endif
+inline void ___rv(){}
+#define vd ___rv()
+#define FA(r,...) if(!(__VA_ARGS__))return r;
